a86ad41fd7aa50437982ce00c7529deeae0b0066
Jose Luis Escorcia C <luix44@gmail.com>
2019-10-23 17:31:04 -0500

    validacion de datos, mensajes de alerta

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,15 @@
                 </v-col>
             </v-row>
             <v-row >
-                    <v-col cols="12" class="align-btns" >
-                        <v-btn color="primary"  @click="buscar" >Buscar</v-btn>
-                        <v-btn color="error"  @click="limpiar" >Limpiar</v-btn>
-                    </v-col>
+                <v-col cols="6">
+                    <v-alert type="info" v-model="msgOK">No se encontro ningun RFC.</v-alert>
+                    <v-alert type="error" v-model="msgWarning">Se encontro {{ countResultados }} RFC.</v-alert>
+                     <v-alert type="warning" v-model="msgError">Pro favor ingrese uno o más RFC en forma de lista</v-alert>
+                </v-col>
+                <v-col cols="6" class="align-btns" >
+                    <v-btn color="primary"  @click="buscar" >Buscar</v-btn>
+                    <v-btn color="error"  @click="limpiar" >Limpiar</v-btn>
+                </v-col>
             </v-row>
             <v-row>
                 <v-col cols="12">
@@ -36,6 +41,10 @@
                 </v-col>
             </v-row>
     </v-container>
+    <v-footer class="footer">
+        <v-spacer></v-spacer>
+        <div>&copy; {{ new Date().getFullYear() }} | Sistemas Grupo VTR</div>
+    </v-footer>
 
   </v-app >
 </template>
@@ -47,6 +56,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 
 #nav {
@@ -64,6 +74,10 @@
 .align-btns{
         display: flex;
         justify-content: flex-end;
+    }
+    .footer{
+        position: absolute;
+        bottom: 0px
     }
 </style>
 																
@@ -93,6 +107,9 @@ export default {
             { text: 'Numero y fecha de oficio global des entencia favorable',                   value: 'Numeroyfechadeoficioglobaldesentenciafavorable',         align: 'left',  sortable: true },
             { text: 'Publicacion DOF sentencia favorable',                                      value: 'PublicacionDOFsentenciafavorable',         align: 'left',  sortable: true },
         ],
+        msgOK: false,
+        msgWarning: false,
+        msgError: false
     }),
     created(){
         //this.loadLista69()
@@ -101,23 +118,49 @@ export default {
     computed:{
         lista69(){
            return this.$store.state.listado69
+        },
+        countResultados(){
+            return this.empresas.length
         }
     },
     methods:{
         //...mapActions('loadLista69'),
         buscar(){
-            
-            var arr = this.RFCs.split(/\r\n|\r|\n/)
-            console.log(arr)
-            arr.forEach(element => {
-                var as = this.lista69.find(item => item.RFC == element)
-                this.empresas.push(as)
-            });
+            this.empresas = []
+            this.msgOK = false
+            this.msgWarning = false
+            this.msgError = false
+            if(this.RFCs !== null && this.RFCs !== ''){
+                console.log(this.RFCs)
+                var arr = this.RFCs.split(/\r\n|\r|\n/)
+                console.log(arr)
+                if(arr.length > 0){
+                    arr.forEach(element => {
+                        var item = this.lista69.find(item => item.RFC == element)
+                        console.log(item)
+                        if(item != undefined)
+                        this.empresas.push(item)
+                    });
+                    if(this.empresas.length > 0){
+                        this.msgWarning = true
+                    }else{
+                        this.msgOK = true
+                    }
+                }else{
+                    this.msgError = true
+                }
+            }else{
+                this.msgError = true
+            }
 
             
         },
         limpiar(){
             this.RFCs = null
+            this.empresas = []
+            this.msgOK = false
+            this.msgWarning = false
+            this.msgError = false
         }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,22 +5,22 @@ import axios from "axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    listado69: []
-  },
-  mutations: {
-    SET_LISTADO69(state, payload) {
-      state.listado69 = payload;
-    }
-  },
-  actions: {
-    loadLista69({ commit }) { 
-      axios.get(`https://raw.githubusercontent.com/jluix44/SAT-Listas-69/master/SAT-Listado-69-B.json`)
-        .then(response => {
-            console.log(response.data);
-            commit("SET_LISTADO69", response.data);
-        }).catch()
-        
-    }
-  },
+    state: {
+        listado69: []
+    },
+    mutations: {
+        SET_LISTADO69(state, payload) {
+        state.listado69 = payload;
+        }
+    },
+    actions: {
+        loadLista69({ commit }) { 
+        axios.get(`https://raw.githubusercontent.com/jluix44/SAT-Listas-69/master/SAT-Listado-69-B.json`)
+            .then(response => {
+                console.log(response.data);
+                commit("SET_LISTADO69", response.data);
+            }).catch()
+            
+        }
+    },
 });
